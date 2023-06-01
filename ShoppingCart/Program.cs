4c72976866eb638be3c9441045aa441ed543fea9
Arham <aarham948@gmail.com>
2023-06-01 03:20:20 +0100

    Step3

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -26,7 +26,6 @@ namespace ShoppingCart
                 get { return price; }
                 set { price = value; }
             }
-
             public int Quantity
             {
                 get { return quantity; }
@@ -38,6 +37,7 @@ namespace ShoppingCart
         {
             //Members
             private List<Product> items;
+            private decimal salesTaxRate;
 
             //Getters&Setters
             public List<Product> Items
@@ -45,13 +45,24 @@ namespace ShoppingCart
                 get { return items; }
                 set { items = value; }
             }
+            public decimal SalesTaxRate
+            {
+                get { return salesTaxRate; }
+                set { salesTaxRate = value; }
+            }
 
             //Constructors
             public ShoppingCart()
             {
                 Items = new List<Product>();
             }
+            public ShoppingCart(decimal salesTaxRate)
+            {
+                this.SalesTaxRate = salesTaxRate;
+                Items = new List<Product>();
+            }
 
+            //Functions
             public decimal CalculateTotalPrice()//Calculate totalprice of shoppingcart without any deductions
             {
                 decimal totalPrice = 0;
@@ -65,13 +76,22 @@ namespace ShoppingCart
 
                 return roundedtotalPrice;
             }
-
+            public decimal CalculateSalesTax()//Sale tax for total price
+            {
+                decimal totalPrice = CalculateTotalPrice();
+                decimal totalTax = totalPrice * salesTaxRate;
+                decimal roundedtotalTax = Decimal.Round(totalTax, 2);
+                return roundedtotalTax;
+            }
         }
         
         static void Main(string[] args)
         {
+            decimal salesTaxRate = 0.125m;
+
             ShoppingCart cart1 = new ShoppingCart();
             ShoppingCart cart2 = new ShoppingCart();
+            ShoppingCart cart3 = new ShoppingCart(salesTaxRate);
 
             //STEP 1:
             Console.WriteLine("Step 1: \n");
@@ -118,6 +138,34 @@ namespace ShoppingCart
 
             //================================================================================//
 
+            //STEP 3:
+            Console.WriteLine("Step 3: \n");
+            //Create Product
+            Product doveSoap3 = new Product
+            {
+                Name = "Dove Soap",
+                Price = 39.99m,
+                Quantity = 2
+            };
+            Product axeDeo3 = new Product
+            {
+                Name = "Axe Deo",
+                Price = 99.99m,
+                Quantity = 2
+            };
+            //Add Items
+            cart3.Items.Add(doveSoap3);
+            cart3.Items.Add(axeDeo3);
+            //Calculations
+            decimal totalPrice3 = cart3.CalculateTotalPrice();
+            decimal totalTax3 = cart3.CalculateSalesTax();
+
+            Console.WriteLine("Shopping Cart:");
+            Console.WriteLine($"- Total Price: {totalPrice3 + totalTax3}");
+            Console.WriteLine($"- Total Tax: {totalTax3}\n");
+
+            //================================================================================//
+            
             Console.ReadLine();
         }
     }
